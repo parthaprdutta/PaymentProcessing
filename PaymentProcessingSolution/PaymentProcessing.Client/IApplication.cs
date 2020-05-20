074063ecdb60dbe8cf84aa06b501f0b5f0d4a2db
ParthaPrDutta <partha.pr.dutta@gmail.com>
2020-05-20 12:26:19 +0530

    Client added

--- PaymentProcessingSolution/PaymentProcessing.Client/IApplication.cs
+++ PaymentProcessingSolution/PaymentProcessing.Client/IApplication.cs
@@ -1,6 +1,9 @@
-﻿namespace PaymentProcessing.Client
+﻿using PaymentProcessing.BusinessRules.Models;
+
+namespace PaymentProcessing.Client
 {
     internal interface IApplication
     {
+        void ProcessPayment(Payment payment);
     }
 }
--- PaymentProcessingSolution/PaymentProcessing.Client/Program.cs
+++ PaymentProcessingSolution/PaymentProcessing.Client/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Autofac;
+using PaymentProcessing.BusinessRules.Enums;
+using PaymentProcessing.BusinessRules.Models;
+using System;
 
 namespace PaymentProcessing.Client
 {
@@ -6,7 +9,15 @@ namespace PaymentProcessing.Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var payment = new Payment(new Product(1, "Microservices", ProductType.Book));
+
+            var container = ContainerConfig.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var application = scope.Resolve<IApplication>();
+                application.ProcessPayment(payment);
+            }
         }
     }
 }
